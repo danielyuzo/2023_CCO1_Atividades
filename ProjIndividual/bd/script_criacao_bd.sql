0c8f05ae24cededcbc2691de955a378045b08cd1
danielyuzo <daniel.palma@sptech.school>
2023-05-11 09:05:50 -0300

    Adição de links para obter posts do banco

--- ProjIndividual/bd/script_criacao_bd.sql
+++ ProjIndividual/bd/script_criacao_bd.sql
@@ -1,14 +1,14 @@
 CREATE DATABASE teste;
 USE teste;
 
+
 CREATE TABLE usuario(
 	idUsuario INT PRIMARY KEY AUTO_INCREMENT,
     username VARCHAR(100) UNIQUE,
     senha VARCHAR(255),
     email VARCHAR(100) UNIQUE,
     dataEntrada DATETIME,
-    imagemPerfil BLOB,
-    administrador BOOLEAN
+    administrador TINYINT
 );
 
 CREATE TABLE post(
@@ -17,7 +17,7 @@ CREATE TABLE post(
     textoPost VARCHAR(500),
     dataCriacao DATETIME,
     dataEdicao DATETIME,
-    fkUsuario INT,
+    fkUsuario INT NOT NULL,
     FOREIGN KEY (fkUsuario) REFERENCES usuario(idUsuario)
 );
 
@@ -29,6 +29,15 @@ CREATE TABLE comentario(
     textoComentario VARCHAR(500),
     dataCriacao DATETIME,
     dataEdicao DATETIME,
-    fkUsuario INT,
+    fkUsuario INT NOT NULL,
     FOREIGN KEY (fkUsuario) REFERENCES usuario(idUsuario)
 );
+
+INSERT INTO usuario VALUES(null, 'Daniel', '12345', 'daniel@hotmail.com', '2023-05-11 08-49-00', 0);
+
+INSERT INTO post VALUES (null, 'post 1', 'Testando sistema de postagens asndkjakjcashwqindoncxzjnaskuqwckjacnkjasn', '2023-05-11 08:31:00', '2023-05-11 08:31:00', 1),
+(null, 'post 2', 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Suscipit at soluta quis officia fugiat labore doloribus atque reprehenderit laboriosam, earum ipsum voluptatibus sint ipsa assumenda neque sed beatae et! Sint.', '2023-05-11 08:31:00', '2023-05-11 08:32:00', 1);
+
+INSERT INTO comentario VALUES (1, 1, 'Comentário teste 1', '2023-05-11 08:49:00', '2023-05-11 08:49:00', 2),
+	(1, 2, 'Comentário teste 2', '2023-05-11 08:52:00', '2023-05-11 08:52:00', 1),
+    (2, 1, 'Comentário teste 3', '2023-05-11 08:31:00', '2023-05-11 08:31:00', 2)
--- ProjIndividual/nodejs/main.js
+++ ProjIndividual/nodejs/main.js
@@ -27,6 +27,36 @@ const servidor = () => {
     app.listen(SERVIDOR_PORTA, () => {
         console.log(`API executada com sucesso na porta ${SERVIDOR_PORTA}`);
     });
+
+    app.get('/listaPosts', (req, res) => {
+        res.sendFile(__dirname + '/listagem.html');
+    })
+
+    app.get('/listaPosts/:id', (req, res) => {
+        res.params = {id: req.params.id};
+        
+        res.sendFile(__dirname + '/post.html');
+    })
+
+    app.get('/posts', (req, res) => {
+        connection.query('SELECT * FROM post', (err, result, fields) => {
+            console.log(fields);
+            console.log('=============');
+            
+            console.log(result);
+            res.send(result);
+        });
+    })
+
+    app.get('/posts/:id', (req, res) => {
+        connection.query('SELECT * FROM post JOIN comentario ON fkPost = idPost WHERE idPost = ?', [req.params.id], (err, result, fields) => {
+            console.log(fields);
+            console.log('=============');
+            
+            console.log(result);
+            res.send(result);
+        });
+    })
     
     app.get('/usuarios', (_, response) => {
         response.sendFile(__dirname + '/index.html');
